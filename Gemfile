701e184c83b42ff3636f340c8f47e7843deecd2e
youyo <1003ni2@gmail.com>
2015-08-06 12:34:10 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'itamae'
+gem 'rake'
+gem 'serverspec'
---
+++ cookbooks/yum/mysql/default.rb
@@ -0,0 +1,12 @@
+case node[:platform_version]
+when /^6/
+  rpm_package_from_url 'http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm' do
+    repo 'mysql56-community'
+  end
+
+when /^7/
+  rpm_package_from_url 'http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm' do
+    repo 'mysql56-community'
+  end
+
+end
