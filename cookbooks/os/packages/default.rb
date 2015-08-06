fdcd95a256e883ed5aee930ffd9aeeadc18b3d55
youyo <1003ni2@gmail.com>
2015-08-06 21:12:45 +0900

    add os/packages

---
+++ cookbooks/os/packages/default.rb
@@ -0,0 +1,24 @@
+node.reverse_merge!({
+  os: {
+    packages: [
+      "wget",
+      "bash-completion",
+      "screen",
+      "sysstat",
+      "dstat",
+      "vim-enhanced",
+      "openssh-server",
+      "git",
+      "libtool",
+      "nc",
+      "sudo",
+      "logrotate",
+      "tar",
+      "ed"
+    ]
+  }
+})
+
+node[:os][:packages].each do |pkg|
+  package pkg
+end
