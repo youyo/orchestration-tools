c2ff08d98f092a0edd03e550a01179a6e5d4b409
youyo <1003ni2@gmail.com>
2015-10-28 15:23:08 +0900

    support amazonlinux

--- cookbooks/yum/nginx/default.rb
+++ cookbooks/yum/nginx/default.rb
@@ -10,3 +10,9 @@ when /^7/
   end
 
 end
+
+if node[:platform] == "amazon"
+  rpm_package_from_url 'http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm' do
+    repo 'nginx'
+  end
+end
