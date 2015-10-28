12f1d08429e6d5053e27c547ed4cabf0ca5f79f1
youyo <1003ni2@gmail.com>
2015-10-28 15:46:50 +0900

    support amazonlinux

--- cookbooks/yum/zabbix/default.rb
+++ cookbooks/yum/zabbix/default.rb
@@ -10,3 +10,9 @@ when /^7/
   end
 
 end
+
+if node[:platform] == "amazon"
+  rpm_package_from_url 'http://repo.zabbix.com/zabbix/2.2/rhel/6/x86_64/zabbix-release-2.2-1.el6.noarch.rpm' do
+    repo 'zabbix'
+  end
+end
