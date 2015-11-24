08622b00148783d6f12f30e7409fadb26c35ddc8
youyo <1003ni2@gmail.com>
2015-11-24 16:15:54 +0900

    add php cookbook.

---
+++ cookbooks/php/php/default.rb
@@ -0,0 +1,41 @@
+node.reverse_merge!({
+  php: {
+    php: {
+      packages: [
+        'php','php-cli','php-common','php-devel','php-fpm','php-gd','php-mbstring','php-mcrypt',
+        'php-mysqlnd','php-pdo','php-pear','php-pecl-jsonc','php-pecl-jsonc-devel',
+        'php-pecl-memcache','php-pecl-zip','php-process','php-xml','php-opcache'
+      ],
+      user: 'apache',
+      fpm: {
+        status: true,
+        listen: '127.0.0.1',
+        pm_max_children: 32,
+        pm_start_servers: 8,
+        pm_min_spare_servers: 8,
+        pm_max_spare_servers: 16
+      }
+    }
+  }
+})
+
+node[:php][:php][:packages].each do |pkg|
+  package pkg do
+    options "--enablerepo=remi,remi-php55"
+    action :install
+  end
+end
+
+execute "sed -i \"s|.*date.timezone =|date.timezone = Asia/Tokyo|g\" /etc/php.ini" do
+  not_if "grep -q -w \"^date.timezone = Asia/Tokyo\" /etc/php.ini"
+end
+
+execute "chown -R #{node[:php][:php][:user]}: /var/lib/php/session/"
+
+if node[:php][:php][:fpm][:status]
+  %w(/etc/php-fpm.conf /etc/php-fpm.d/www.conf).each do |f|
+    template f do
+      notifies :restart, "service[php-fpm]"
+    end
+  end
+end
